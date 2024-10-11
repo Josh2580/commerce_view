176a7e883da0b4cce0d98ee46489bb4a4ff68832
Josh2580 <joshuaugwuede@gmail.com>
2024-10-11 07:12:30 -0700

    removed unused declaration

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -7,8 +7,6 @@ import Product2 from "../assets/product-2.jpg";
 import Product3 from "../assets/product-3.jpg";
 import Product4 from "../assets/product-4.jpg";
 
-const images = [Product1, Product2, Product3, Product4];
-
 const featuredProducts = [
   {
     id: 1,
